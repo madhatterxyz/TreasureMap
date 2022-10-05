6b43770b341bd32a406f46cd788002f9d150e1ee
Nicolas <Nicolas@PC-NICOLAS>
2022-10-05 09:43:11 +0200

    test mapParameters isValid - refactor

--- TM.Domain/MapDomain.cs
+++ TM.Domain/MapDomain.cs
@@ -5,8 +5,27 @@
         public bool IsValid(string mapEntries)
         {
             string[] entries = mapEntries.Split(" - ");
-            return entries.Length == 3 && (entries[0].Equals("C") || entries[0].Equals("M")) && 
-                (int.Parse(entries[1]) > int.Parse(entries[2]) || int.Parse(entries[1]) < int.Parse(entries[2])) ;
+            if (entries.Length != 3)
+            {
+                return false;
+            }
+            switch (entries[0])
+            {
+                case "C":
+                    if (int.Parse(entries[1]) > int.Parse(entries[2]) || int.Parse(entries[1]) < int.Parse(entries[2]))
+                    {
+                        return true;
+                    }
+                    break;
+                case "M":
+                    if (!int.TryParse(entries[1], out _) || !int.TryParse(entries[2], out _))
+                    {
+                        return false;
+                    }
+                    break;
+                default: return false;
+            }
+            return true;
         }
     }
 }
