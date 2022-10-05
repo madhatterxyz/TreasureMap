d0fb17446d629df54dbf9317279080265d82775e
Nicolas <Nicolas@PC-NICOLAS>
2022-10-05 10:06:53 +0200

    treasure validity - green

--- TM.Domain/MapDomain.cs
+++ TM.Domain/MapDomain.cs
@@ -5,20 +5,22 @@
         public bool IsValid(string mapEntries)
         {
             string[] entries = mapEntries.Split(" - ");
-            if (entries.Length != 3)
-            {
-                return false;
-            }
             switch (entries[0])
             {
                 case "C":
-                    if (int.Parse(entries[1]) > int.Parse(entries[2]) || int.Parse(entries[1]) < int.Parse(entries[2]))
+                    if (entries.Length != 3 && (int.Parse(entries[1]) > int.Parse(entries[2]) || int.Parse(entries[1]) < int.Parse(entries[2])))
                     {
                         return true;
                     }
                     break;
                 case "M":
-                    if (!int.TryParse(entries[1], out _) || !int.TryParse(entries[2], out _))
+                    if (entries.Length != 3 && (!int.TryParse(entries[1], out _) || !int.TryParse(entries[2], out _)))
+                    {
+                        return false;
+                    }
+                    break;
+                case "T":
+                    if (entries.Length != 4 && (!int.TryParse(entries[1], out _) || !int.TryParse(entries[2], out _) || !int.TryParse(entries[3], out _)))
                     {
                         return false;
                     }
