d23f260227ad6857348fd61dbd31248e766c43b3
Nicolas <Nicolas@PC-NICOLAS>
2022-10-05 09:35:07 +0200

    test mountain is valid - red

--- TM.Domain/MapDomain.cs
+++ TM.Domain/MapDomain.cs
@@ -5,7 +5,8 @@
         public bool IsValid(string mapEntries)
         {
             string[] entries = mapEntries.Split(" - ");
-            return entries.Length == 3 && entries[0].Equals("C") ;
+            return entries.Length == 3 && (entries[0].Equals("C") || entries[0].Equals("M")) && 
+                (int.Parse(entries[1]) > int.Parse(entries[2]) || int.Parse(entries[1]) < int.Parse(entries[2])) ;
         }
     }
 }
--- TM.Tests/MapTests.cs
+++ TM.Tests/MapTests.cs
@@ -14,5 +14,16 @@ namespace TM.Tests
             //Assert
             Assert.True(actualResult);
         }
+
+        [Fact]
+        public void MountainParameters_IsValid_True()
+        {
+            //Arrange
+            MapDomain domain = new MapDomain();
+            //Act
+            bool actualResult = domain.IsValid("M - 1 - 1");
+            //Assert
+            Assert.True(actualResult);
+        }
     }
 }
